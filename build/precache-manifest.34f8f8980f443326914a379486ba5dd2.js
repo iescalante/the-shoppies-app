93369f4db94d4ca70f126033f857b89072182c9e
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-09-07 22:46:29 -0400

    created build for deployment

---
+++ build/precache-manifest.34f8f8980f443326914a379486ba5dd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d42aca8688cad84f1b51eb88238b8c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b645f833e4afb0f2ec67",
+    "url": "/static/js/2.51b2e4b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.51b2e4b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e8f960cc8de4d109e43",
+    "url": "/static/js/main.1a444fed.chunk.js"
+  },
+  {
+    "revision": "1512875dc019d19a0767",
+    "url": "/static/js/runtime-main.79cc937a.js"
+  }
+]);
